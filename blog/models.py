140029f0f55070cf0c4b6a7404240d06d66921dd
alizafar96 <alizafarthadexter@gmail.com>
2017-07-15 02:15:06 +0500

    Blog, first commit

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=300)
+    text = models.TextField()
+    creation_date = models.DateTimeField(default=timezone.now())
+    publish_date = models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        self.publish_date=timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
